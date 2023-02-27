43bdc53c390890dd3b63c20e17c49d0c6ef54a36
GabCopriva <contatogabrielcopriva@gmail.com>
2023-02-27 08:52:58 -0300

    Atualizado

--- src/healthCare/model/Doadora.java
+++ src/healthCare/model/Doadora.java
@@ -3,7 +3,7 @@ public class Doadora extends Pessoa{
 
 	public Doadora(int id, String nome, int idade, double peso, Boolean jaTeveDoenca, Boolean estaGravida) {
 		super(id, nome, idade, peso, jaTeveDoenca, estaGravida);
-		// TODO Auto-generated constructor stub
+		
 	}
 	
 }
